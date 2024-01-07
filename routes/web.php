3e8ab0d41adb32dcbf36ead3e800dbb8483826b1
Danillo Rodrigues <dandrn7@gmail.com>
2024-01-07 13:24:30 -0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
-use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get("/inicio", [MainController::class, "index"])->name("index");
-
-/**
- * Rotas para teste
- */
-Route::get('/teste', function () {
-    return phpinfo();
+Route::get("/", function () {
+    return view("index");
 });
