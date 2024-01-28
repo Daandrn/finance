b6eaea114cbc46cbea8898bf6b6009969c721fca
Danillo Rodrigues <dandrn7@gmail.com>
2024-01-27 22:16:24 -0300

    wip: implementacao de sessao administrador, usuarios

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DTO\UserUpdateDTO;
+use App\Http\Requests\UserAdmRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(User $user)
+    {
+        $users = $user->select($columns = ['id', 'name', 'email', 'adm', 'status', 'created_at'])
+                        ->orderBy('id')
+                        ->paginate(15);
+
+        return view('administrator.users', compact('users'));
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store(User $user)
+    {
+        # code...
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user, int $id)
+    {
+        $users = $user->find($id, ['id', 'name', 'email', 'adm', 'status']);
+
+        return view('administrator.alterUser', compact('users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(User $user, int $id, UserAdmRequest $request)
+    {
+        $users = $user->find($id);
+        $users->update((array) UserUpdateDTO::userDTO($request));
+
+        return redirect()
+                ->route('users')
+                ->with(["status" => "200", "message" => "sucess"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user, int $id)
+    {
+        $user->destroy($id);
+
+        return redirect()
+                ->Route('users')
+                ->with(["status" => "200", "message" => "sucess"]);
+    }
+}
