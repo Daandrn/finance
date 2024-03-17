dcc13d3a7ed3a232dca427182722f25b903c995f
Danillo Rodrigues <dandrn7@gmail.com>
2024-03-17 16:52:40 -0300

    wip: implementa retorno de tipo de titulo.

---
+++ app/Models/TitleType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TitleType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'has_irpf',
+    ];
+
+    public function titles(): HasMany
+    {
+        return $this->hasMany(Title::class);
+    }
+}
