d8444f4aa722e9feac7799d05fe6e3b7e0b06cbe
Danillo Rodrigues <dandrn7@gmail.com>
2024-10-10 21:38:57 -0300

    chore: implements Brapi stocks api

--- app/RepositoriesApi/StocksApiInterface.php
+++ app/RepositoriesApi/StocksApiInterface.php
@@ -2,7 +2,14 @@
 
 namespace App\RepositoriesApi;
 
+use App\Repositories\StocksRepository;
+use Illuminate\Http\Client\Factory;
+
 interface StocksApiInterface
 {
+    public function __construct(Factory $http, StocksRepository $stocksRepository);
     public function getCurrentValue(string $ticker): string|null;
+    public function getStocksValues(array $stocks): array|null;
+    public function errorExists(): bool;
+    public function errorMessage(): string;
 }
